200fc1613f4e8a41d548d005c7935e261d0c10e8
Mattique <mr.unknown5264@gmail.com>
2025-01-16 14:50:03 +0500

    make login and signup using shadcn ui and make server file

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import HeroSection from "./pages/student/HeroSection"
+import Login from "./pages/Login"
+
+function App() {
+
+
+  return (
+    <>
+   <main>
+    <Login/>
+   </main>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,143 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+
+export default function Login() {
+  const [loginInput, setLoginInput] = useState({ email: "", password: "" });
+  const [signupInput, setSignupInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e, type) => {
+    if (type === "login") {
+      setLoginInput({ ...loginInput, [e.target.name]: e.target.value });
+      // console.log(loginInput)
+    } else {
+      setSignupInput({ ...signupInput, [e.target.name]: e.target.value });
+      // console.log(signupInput)
+    }
+
+  };  
+    const handleRegistration = (type) => {
+      const inputDataResult = type === "login" ? loginInput : signupInput;
+      console.log(inputDataResult);
+    };
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">SignUp</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-center text-xl ">Login</CardTitle>
+              <CardDescription>
+                Login to your account. Enter your email and password to continue
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Email</Label>
+                <Input
+                  id="email"
+                  value={loginInput.email}
+                  onChange={(e) => handleChange(e, "login")}
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="Jhon@gmail.com"
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  placeholder="123..."
+                  required
+                  name="password"
+                  value={loginInput.password}
+                  onChange={(e) => handleChange(e, "login")}
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button
+                className="w-full"
+                onClick={() => handleRegistration("login")}
+              >
+                Login
+              </Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="signup">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-center text-xl ">SignUp</CardTitle>
+              <CardDescription>
+                Create a new account here. Click signup when you're done.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="Jhon Doe"
+                  name="username"
+                  value={signupInput.username}
+                  onChange={(e) => handleChange(e, "signup")}
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="Jhon@gmail.com"
+                  name="email"
+                  value={signupInput.email}
+                  onChange={(e) => handleChange(e, "signup")}
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="123"
+                  name="password"
+                  value={signupInput.password}
+                  onChange={(e) => handleChange(e, "signup")}
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button
+                className="w-full"
+                onClick={() => handleRegistration("signup")}
+              >
+                Sign Up
+              </Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
