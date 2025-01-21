49391817b493b75db20cede4da864e74491fb44a
Mattique <mr.unknown5264@gmail.com>
2025-01-21 20:54:23 +0500

    add update and remove lecture functionality with appropriate routes and authentication

--- server/controllers/lecture.controller.js
+++ server/controllers/lecture.controller.js
@@ -1,5 +1,6 @@
 import Course from "../models/course.model.js";
 import Lecture from "../models/lecture.model.js";
+import { deleteVideoFromCloudinary } from "../utils/cloudinary.js";
 
 export const createLectureController = async (req, res) => {
   try {
@@ -68,5 +69,99 @@ export const getListOfLecturesController = async (req, res) => {
       message: "Error in get list of lectures controller",
     });
     
+  }
+}
+export const updateLecutreController= async(req,res)=>{
+  try {
+    const {isPreviewFree, videoInfo,title} = req.body
+    const {courseId, lectureId} = req.params
+
+    const lecture = await Lecture.findByIdAndUpdate(lectureId)
+
+    if(title) lecture.title = title
+    if(isPreviewFree) lecture.isPreviewFree = isPreviewFree
+    if(videoInfo.videoUrl) lecture.videoUrl = videoInfo.videoUrl
+    if(videoInfo.publicId) lecture.publicId = videoInfo.publicId
+    await lecture.save()
+
+    const course = await Course.findById(courseId)
+
+    if(course && course.lectures.includes(lecture._id)){
+      course.lectures.push(lecture._id)
+      await course.save()
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Lecture Updated Successfuly",
+      lecture
+    })
+  } catch (error) {
+    console.log("Error in update lecture controller", error)
+
+    res.status(500).json({
+      success: false,
+      message: "Error in update lecture controller "
+    })
+  }
+}
+
+
+export const  removeLectureController = async( req, res)=>{
+try {
+  const { lectureId} = req.params
+  const lecture = await Lecture.findByIdAndDelete(lectureId)
+  if(!lecture){
+    return res.status(400).json({
+      success: false,
+      message: "Lecture not found",
+    });
+  }
+// Remove from cloudinay
+   if(lecture.publicId){
+    await deleteVideoFromCloudinary(lecture.publicId)
+   }
+
+  //  remove the refrence from the course 
+  await Course.updateOne({lectures: lectureId}, {$pull: {lectures: lectureId}})
+
+  res.status(200).json({
+    success: true,
+    message: "Lecture Deleted Successfully",
+    lecture
+  }
+  )
+} catch (error) {
+  console.log("Error in remove lecture controller", error);
+  res.status(500).json({
+    success: false,
+    message: "Error in remove lecture controller",
+  });
+}
+} 
+
+
+
+export const getLectureById = async(req, res)=>{
+  try {
+    const {lectureId}= req.params
+    const lecture = await Lecture.findById(lectureId)
+    if(!lecture){
+      return res.status(400).json({
+        success: false,
+        message: "Lecture not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Lecture Get By Id successfuly",
+      lecture
+    })
+  } catch (error) {
+    console.log("Error in get Lectures By Id Controller",error)
+    return res.status(400).json({
+      success: false,
+      message: "Error in get Lectures By Id Controller",
+    });
   }
 }
